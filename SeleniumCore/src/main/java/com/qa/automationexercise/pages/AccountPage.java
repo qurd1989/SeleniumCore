d52a0154a4047ab5fb28003c5dc40e88b17fbba0
Qurd1989 <mashaedu@gmail.com>
2025-07-01 23:45:54 -0400

    Xpath Updated!

--- SeleniumCore/src/main/java/com/qa/automationexercise/pages/AccountPage.java
+++ SeleniumCore/src/main/java/com/qa/automationexercise/pages/AccountPage.java
@@ -18,7 +18,7 @@ public class AccountPage {
 	private By loggedInAs = By.xpath("//*[contains(text(), 'Logged in as')]");
 	private By logo = By.xpath("//img[@src='/static/images/home/logo.png']");
 	private By testCaseLink = By.xpath("//*[@class='btn btn-success' and text()='Test Cases']");
-	private By apisListForPraciteLink = By.xpath("//*[contains(text(), 'Logged in as')]");
+	private By apisListForPraciteLink = By.xpath("//*[@class='btn btn-success' and text()='APIs list for practice']");
 	private By headers = By.cssSelector("ul.navbar-nav > li");
 	
 	public AccountPage(WebDriver driver) {

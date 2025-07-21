c65c7b2162761a98e440956c1a2bca802ec529b8
Qurd1989 <mashaedu@gmail.com>
2025-07-20 23:54:51 -0400

    POM file updates!

--- SeleniumCore/src/test/java/com/qa/automationexercise/tests/TestRunner.java
+++ SeleniumCore/src/test/java/com/qa/automationexercise/tests/TestRunner.java
@@ -1,21 +1,19 @@
 package com.qa.automationexercise.tests;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(
-//		features = "classpath:parallel",
-//		glue = "stepDefinitions",
-//		plugin = {
-//				"pretty",
-//				"html:target/cucumber-reports.html",
-//				"json:target/cucumber-reports.json"
-//		},
-//		monochrome = true
-//		)
-//public class TestRunner {
-//
-//}
+
+@CucumberOptions(
+		features = "classpath:parallel",
+		glue = "stepDefinitions",
+		plugin = {
+				"pretty",
+				"html:target/cucumber-reports.html",
+				"json:target/cucumber-reports.json"
+		},
+		monochrome = true
+		)
+public class TestRunner extends AbstractTestNGCucumberTests{
+
+}

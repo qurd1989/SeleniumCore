e452f7fd48228df2782c12142e6b1022008a420e
Qurd1989 <mashaedu@gmail.com>
2025-08-14 23:35:58 -0400

    new comment added

--- SeleniumCore/src/main/java/com/qa/automationexercise/exceptions/BrowserExeption.java
+++ SeleniumCore/src/main/java/com/qa/automationexercise/exceptions/BrowserExeption.java
@@ -1,6 +1,11 @@
 package com.qa.automationexercise.exceptions;
 
 public class BrowserExeption extends RuntimeException {
+    /* * Custom exception class for handling browser-related errors.
+ * This class extends RuntimeException and can be used to throw exceptions
+ * related to browser operations, such as initialization failures or
+ * unsupported browser types.
+     */
 	public BrowserExeption(String mesg) {
 		super(mesg);
 	}

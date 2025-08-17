10387af17f79e1fbaba4e6b4bf57c37d4d0bdc18
Qurd1989 <mashaedu@gmail.com>
2025-08-16 23:48:09 -0400

    new commit

--- SeleniumCore/src/main/java/com/qa/automationexercise/exceptions/FrameworkException.java
+++ SeleniumCore/src/main/java/com/qa/automationexercise/exceptions/FrameworkException.java
@@ -2,6 +2,12 @@ package com.qa.automationexercise.exceptions;
 
 public class FrameworkException extends RuntimeException{
 
+    /** * Custom exception class for handling framework-related errors.
+     * This class extends RuntimeException and can be used to throw exceptions
+     * related to framework operations, such as configuration issues or
+     * initialization failures.
+     *
+     */
 	public FrameworkException(String msg) {
 		super(msg);
 		
